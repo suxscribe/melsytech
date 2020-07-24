eecc1f9c613651cfe1277e8d8b782f86a7df8f2f
Sergei Birukov <suxscribe@gmail.com>
2020-07-24 09:23:40 +0300

    index page + package updates

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
+import Swiper, { Navigation, EffectFade, Keyboard, Controller } from 'swiper';
+import 'simplebar'; // or "import SimpleBar from 'simplebar';" if you want to use it manually.
+Swiper.use([Navigation, EffectFade, Keyboard, Controller]);
 
 window.UIkit = UIkit; // fix not difined bug
 
@@ -20,3 +23,95 @@ requireAll(require.context('../svg/', true, /\.svg$/));
 //     e.target.classList.add('classssss');
 //   });
 // });
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (document.querySelector('.page--index')) {
+    var swiperProcedures = new Swiper('.procedures__slider', {
+      effect: 'fade',
+      fadeEffect: {
+        crossFade: true,
+      },
+
+      loop: true,
+      loopedSlides: 6,
+      slidesPerView: '1',
+      grabCursor: false,
+      allowTouchMove: false,
+      navigation: {
+        nextEl: '.procedures__slider-nav--right',
+        prevEl: '.procedures__slider-nav--left',
+      },
+      keyboard: true,
+      onSlideChangeEnd: function(e) {
+        console.log(e);
+      },
+    });
+
+    var swiperProceduresItems = new Swiper('.procedures__slider-right', {
+      effect: 'slide',
+
+      loop: true,
+      loopedSlides: 6,
+      slidesPerView: 'auto',
+    });
+
+    // control each swiper
+    swiperProcedures.controller.control = swiperProceduresItems;
+    swiperProceduresItems.controller.control = swiperProcedures;
+
+    var swiperReviews = new Swiper('.reviews__right-slider', {
+      effect: 'fade',
+      fadeEffect: {
+        crossFade: true,
+      },
+
+      loop: true,
+      slidesPerView: '1',
+      grabCursor: false,
+      allowTouchMove: true,
+      navigation: {
+        nextEl: '.reviews__slider-nav--right',
+        prevEl: '.reviews__slider-nav--left',
+      },
+      keyboard: true,
+    });
+
+    // document.querySelector('.partners__item-link').addEventListener('click', e => {
+    //   document.querySelectorAll('.partners__item-drop').forEach(el => {
+    //     el.classList.remove('drop--active');
+    //   })
+    // });
+
+    var swiperNews = new Swiper('.news__slider', {
+      effect: 'slide',
+      fadeEffect: {
+        crossFade: true,
+      },
+
+      loop: true,
+      slidesPerView: 'auto',
+      allowTouchMove: true,
+      navigation: {
+        nextEl: '.news__slider-nav--right',
+        prevEl: '.news__slider-nav--left',
+      },
+      keyboard: true,
+    });
+
+    function partnersRemoveActive() {
+      document.querySelectorAll('.partners__right .uk-active').forEach((el) => {
+        el.classList.remove('uk-active');
+        // el.classList.add('removed');
+        console.log('remove active');
+      });
+    }
+    document.querySelectorAll('.partners__item-drop-close').forEach((el) => {
+      el.addEventListener('click', (e) => {
+        e.preventDefault();
+        partnersRemoveActive();
+      });
+    });
+
+    partnersRemoveActive();
+  } // page--index
+});
